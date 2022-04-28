38875dab84747caaf482b10d48e846307c14755a
Blake2912 <kcvarun29122001@gmail.com>
2022-04-28 20:18:41 +0530

    Initial Commit

---
+++ Pac-Man-Open-GL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Pac-Man-Open-GL
+//
+//  Created by K C Varun on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
